5f2c89e2a63adce38637913b76288475834c59b2
robrechtme <robrechtmeersman@hotmail.com>
2022-01-21 18:46:53 +0100

    fix: import font

--- app/root.tsx
+++ app/root.tsx
@@ -27,6 +27,16 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;800&display=swap"
+          rel="stylesheet"
+        ></link>
         <Meta />
         <Links />
       </head>
