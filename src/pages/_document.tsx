dd4605b8d40b618052e09018fadf0c25ee2162c2
Robrecht Meersman <robrechtmeersman@hotmail.com>
2022-03-05 11:54:11 +0100

    feat: add SEO

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,31 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="nl">
       <Head>
+        <title>Tweede kookboek van Robrecht</title>
+        <meta name="title" content="Tweede kookboek van Robrecht" />
+        <meta
+          name="description"
+          content="Een collectie van lekkere recepten, deze keer niet van mezelf"
+        />
+        <meta name="keywords" content="kookboek,recepten,koken,robrecht" />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="revisit-after" content="30 days" />
+        <meta name="author" content="Robrecht Meersman" />
+        <meta property="og:title" content="Tweede kookboek van Robrecht" />
+        <meta property="og:site_name" content="kookboek.robrecht.me" />
+        <meta property="og:url" content="https://kookboek.robrecht.me/" />
+        <meta
+          property="og:description"
+          content="Een collectie van lekkere recepten, deze keer niet van mezelf "
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://kookboek.robrecht.me/og-image.png"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
