3b7e8008eb6e7974123caa8d38ffa42e295be0fb
robrechtme <robrechtmeersman@hotmail.com>
2026-02-22 17:31:06 +0100

    feat: add orzo met courgette, tomaat en spinazie recipe
    Imported from secretosdelsur.com with added berloumi as grilled topping.

--- data/index.ts
+++ data/index.ts
@@ -1,39 +1,44 @@
-import aardappelOvenschotelMetLinzenEnKikkererwtenImage from './aardappel-ovenschotel-met-linzen-en-kikkererwten/image.webp';
+import { Recipe } from '@core/types';
+
+// Import all recipe data
 import aardappelOvenschotelMetLinzenEnKikkererwtenRecipe from './aardappel-ovenschotel-met-linzen-en-kikkererwten/recipe.json';
-import chiliSinCarneImage from './chili-sin-carne/image.webp';
 import chiliSinCarneRecipe from './chili-sin-carne/recipe.json';
-import creamyPastaMetKerstomatensausEnRicottaImage from './creamy-pasta-met-kerstomatensaus-en-ricotta/image.webp';
 import creamyPastaMetKerstomatensausEnRicottaRecipe from './creamy-pasta-met-kerstomatensaus-en-ricotta/recipe.json';
-import gnocchiInRomigeTomatensausImage from './gnocchi-in-romige-tomatensaus/image.webp';
 import gnocchiInRomigeTomatensausRecipe from './gnocchi-in-romige-tomatensaus/recipe.json';
-import gnocchiMetBloemkoolEnPaprikasausUitDeOvenImage from './gnocchi-met-bloemkool-en-paprikasaus-uit-de-oven/image.webp';
 import gnocchiMetBloemkoolEnPaprikasausUitDeOvenRecipe from './gnocchi-met-bloemkool-en-paprikasaus-uit-de-oven/recipe.json';
-import griekseMoussakaImage from './griekse-moussaka/image.webp';
 import griekseMoussakaRecipe from './griekse-moussaka/recipe.json';
-import kipTikkaMasalaImage from './kip-tikka-masala/image.webp';
 import kipTikkaMasalaRecipe from './kip-tikka-masala/recipe.json';
-import lasagneMetGegrildeGroentenImage from './lasagne-met-gegrilde-groenten/image.webp';
 import lasagneMetGegrildeGroentenRecipe from './lasagne-met-gegrilde-groenten/recipe.json';
-import ovenschotelMetKipEnItaliaanseGroentenImage from './ovenschotel-met-kip-en-italiaanse-groenten/image.webp';
+import orzoMetCourgetteTomaatEnSpinazieRecipe from './orzo-met-courgette-tomaat-en-spinazie/recipe.json';
 import ovenschotelMetKipEnItaliaanseGroentenRecipe from './ovenschotel-met-kip-en-italiaanse-groenten/recipe.json';
-import ovenschotelMetVeggieGehaktEnBroccoliImage from './ovenschotel-met-veggie-gehakt-en-broccoli/image.webp';
 import ovenschotelMetVeggieGehaktEnBroccoliRecipe from './ovenschotel-met-veggie-gehakt-en-broccoli/recipe.json';
-import parelcouscousMetKerstomaatjesEnHalloumiImage from './parelcouscous-met-kerstomaatjes-en-halloumi/image.webp';
 import parelcouscousMetKerstomaatjesEnHalloumiRecipe from './parelcouscous-met-kerstomaatjes-en-halloumi/recipe.json';
-import pastaMetScampiInTomatenroomsausImage from './pasta-met-scampi-in-tomatenroomsaus/image.webp';
 import pastaMetScampiInTomatenroomsausRecipe from './pasta-met-scampi-in-tomatenroomsaus/recipe.json';
-import pastagratinMetBoontjesEnBroccolisausImage from './pastagratin-met-boontjes-en-broccolisaus/image.webp';
 import pastagratinMetBoontjesEnBroccolisausRecipe from './pastagratin-met-boontjes-en-broccolisaus/recipe.json';
-import pastitsioImage from './pastitsio/image.webp';
 import pastitsioRecipe from './pastitsio/recipe.json';
-import quicheMetPreiZalmEnGeitenkaasImage from './quiche-met-prei-zalm-en-geitenkaas/image.webp';
 import quicheMetPreiZalmEnGeitenkaasRecipe from './quiche-met-prei-zalm-en-geitenkaas/recipe.json';
-import risottoMetPestoCourgetteEnHalloumiImage from './risotto-met-pesto-courgette-en-halloumi/image.webp';
 import risottoMetPestoCourgetteEnHalloumiRecipe from './risotto-met-pesto-courgette-en-halloumi/recipe.json';
-import zalmSpinazieLasagneImage from './zalm-spinazie-lasagne/image.webp';
 import zalmSpinazieLasagneRecipe from './zalm-spinazie-lasagne/recipe.json';
 
-import { Recipe } from '@core/types';
+// Import all recipe images
+import aardappelOvenschotelMetLinzenEnKikkererwtenImage from './aardappel-ovenschotel-met-linzen-en-kikkererwten/image.webp';
+import chiliSinCarneImage from './chili-sin-carne/image.webp';
+import creamyPastaMetKerstomatensausEnRicottaImage from './creamy-pasta-met-kerstomatensaus-en-ricotta/image.webp';
+import gnocchiInRomigeTomatensausImage from './gnocchi-in-romige-tomatensaus/image.webp';
+import gnocchiMetBloemkoolEnPaprikasausUitDeOvenImage from './gnocchi-met-bloemkool-en-paprikasaus-uit-de-oven/image.webp';
+import griekseMoussakaImage from './griekse-moussaka/image.webp';
+import kipTikkaMasalaImage from './kip-tikka-masala/image.webp';
+import lasagneMetGegrildeGroentenImage from './lasagne-met-gegrilde-groenten/image.webp';
+import orzoMetCourgetteTomaatEnSpinazieImage from './orzo-met-courgette-tomaat-en-spinazie/image.webp';
+import ovenschotelMetKipEnItaliaanseGroentenImage from './ovenschotel-met-kip-en-italiaanse-groenten/image.webp';
+import ovenschotelMetVeggieGehaktEnBroccoliImage from './ovenschotel-met-veggie-gehakt-en-broccoli/image.webp';
+import parelcouscousMetKerstomaatjesEnHalloumiImage from './parelcouscous-met-kerstomaatjes-en-halloumi/image.webp';
+import pastaMetScampiInTomatenroomsausImage from './pasta-met-scampi-in-tomatenroomsaus/image.webp';
+import pastagratinMetBoontjesEnBroccolisausImage from './pastagratin-met-boontjes-en-broccolisaus/image.webp';
+import pastitsioImage from './pastitsio/image.webp';
+import quicheMetPreiZalmEnGeitenkaasImage from './quiche-met-prei-zalm-en-geitenkaas/image.webp';
+import risottoMetPestoCourgetteEnHalloumiImage from './risotto-met-pesto-courgette-en-halloumi/image.webp';
+import zalmSpinazieLasagneImage from './zalm-spinazie-lasagne/image.webp';
 
 // Export recipes array
 export const recipes: Recipe[] = [
@@ -45,6 +50,7 @@ export const recipes: Recipe[] = [
   griekseMoussakaRecipe as Recipe,
   kipTikkaMasalaRecipe as Recipe,
   lasagneMetGegrildeGroentenRecipe as Recipe,
+  orzoMetCourgetteTomaatEnSpinazieRecipe as Recipe,
   ovenschotelMetKipEnItaliaanseGroentenRecipe as Recipe,
   ovenschotelMetVeggieGehaktEnBroccoliRecipe as Recipe,
   parelcouscousMetKerstomaatjesEnHalloumiRecipe as Recipe,
@@ -66,6 +72,7 @@ export const images = {
   'griekse-moussaka': griekseMoussakaImage,
   'kip-tikka-masala': kipTikkaMasalaImage,
   'lasagne-met-gegrilde-groenten': lasagneMetGegrildeGroentenImage,
+  'orzo-met-courgette-tomaat-en-spinazie': orzoMetCourgetteTomaatEnSpinazieImage,
   'ovenschotel-met-kip-en-italiaanse-groenten': ovenschotelMetKipEnItaliaanseGroentenImage,
   'ovenschotel-met-veggie-gehakt-en-broccoli': ovenschotelMetVeggieGehaktEnBroccoliImage,
   'parelcouscous-met-kerstomaatjes-en-halloumi': parelcouscousMetKerstomaatjesEnHalloumiImage,
